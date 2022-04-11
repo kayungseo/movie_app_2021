81094237f55a8fe705cfc2fb95c608007245d2df
sky <sgy0202@i-one.co.kr>
2022-04-11 14:48:48 +0900

    css파일 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
+
 class App extends React.Component {
 
   state = {
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import './Movie.css';
 
 function Movie({title, year, summary, poster, genres}){ //state 필요없음 => 함수형 컴포넌트로 작성 
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title}/>
       <div class="movie__data">
-        <h3 class="movie__title" style={{backgroundColor: 'red'}}>{title}</h3>
+        <h3 class="movie__title">{title}</h3>
         <h5 class="movie__year">{year}</h5>
         <p class="movie__summary">{summary}</p>
       </div>
